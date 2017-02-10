32160083a19c0467bd194dbc0198544ec28ffcc0
Alex Greene <alexgrn7@gmail.com>
2017-02-10 01:14:40 -0800

    Using NLTK to tokenize and chunk wikipedia text. The route I have decided to take involves looking at the parts of speech within a sentence to see if there is potential for a question to be formed. Today I spend almost allof my time tinkering with the grammar for the NLTK RegexpParser. Tomorrow I will have the questions actually generate from the chunker output, and display the questions on the web page.

--- python/Article.py
+++ python/Article.py
@@ -1,6 +1,7 @@
 import wikipedia as wiki
-from textblob import TextBlob
-import math
+import nltk
+from nltk.tokenize import sent_tokenize
+import re
 
 from Quiz import Quiz
 from Question import Question
@@ -9,7 +10,66 @@ class Article():
 
     def __init__ (self, name):
         self.name = name
-        summary = wiki.page(name).summary
+        page = wiki.page(name)
+        summary = page.summary.encode('ascii', 'ignore')
+
+        _sections = page.sections
+
+        sections = []
+        for section in _sections:
+            sec = page.section(section)
+            if sec is not None:
+                processed = self.process_section(page.section(section))
+                if len(processed) > 0:
+                    sections.append(
+                        (section, processed))
+
+        print "-----------------------------"
+        print "-----------------------------"
+        print "-----------------------------"
+        print "\n"
+
+        for word in sections[1][1]:
+            print word
+
+    def process_section(self, s):
+        s = s.replace(")", "(")
+        s = re.split('\(', s)
+        s = "".join(s[0::2])
+        _sentences = sent_tokenize(s)
+
+        sentences = []
+        for sent in _sentences:
+            sentences.append(
+                self.chunk_sentence(sent))
+
+        return sentences
+
+    def chunk_sentence(self, s):
+
+        tokens = nltk.word_tokenize(s)
+        tagged = nltk.pos_tag(tokens)
+        grammar = """  
+                    NUMBER: {<$>*<CD>+<NN>*}
+                    DATE: {<IN><NUMBER>}
+                    LOCATION: {<NNP>+<,><NNP>+}
+                    PROPER: {<NNP|NNPS>+}
+                    PROPER_FOR_MATCH: {(<NNP|NNPS>+<NN>?(<CC>?<DT>?<IN>?<NNP|NN>)+)+}
+                    PROPER_LIST: {(<PROPER><CC><PROPER><CC>?)+}
+                    THING: {<JJ|JJR>+<NN|NNS>+<CC>?<NN>*}
+                    MATCH: {<THING><.*>?<.*>?<PROPER_FOR_MATCH>+}
+
+                    HIT!: {<PROPER><NN>?<VBZ|VBN>+}
+                     """            
+                    
+        # HIT and MATCH will be used for generating questions
+                    
+        chunker = nltk.RegexpParser(grammar)
+        result = chunker.parse(tagged)
+
+        return result
+
+
 
     def build_quiz(self):
         first_question = Question(
@@ -20,8 +80,4 @@ class Article():
             })
 
         self.quiz = Quiz([first_question])
-
-
-
-
 
