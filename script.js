76b4fbfe27fd031ce3ee172fb42a5fc9bdf61175
Alex Greene <alexgrn7@gmail.com>
2017-02-10 21:52:51 -0800

    Answers now appear, and the web app provides feedback on whether or not the answer was correct. Next step is to hide the answer from the question text, aka create the blank to fill.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var article_name = document.getElementById('article_name');
 var question_body = document.getElementById('question_body');
 var search = document.getElementById('article_name_input');
+var next_question_btn = document.getElementById('next_question_btn');
+var answer_a = document.getElementById('answer_a');
+var answer_b = document.getElementById('answer_b');
+var answer_c = document.getElementById('answer_c');
+var answer_response_label = document.getElementById('answer_response_label');
+    answer_response_label.style.display = "none";
+    answer_a.style.display = "none";
+    answer_b.style.display = "none";
+    answer_c.style.display = "none";
+    next_question_btn.style.display = "none";
 
 var data;
 var q_idx = 0;
 var curQuery;
+var correct_answer;
 
 function new_article_query(_data) {
     if (!_data) {
@@ -30,12 +41,86 @@ function make_request(query, _callback) {
 }
 
 function load_question() {
+    answer_response_label.style.display = "none";
+    answer_a.style.display = "block";
+    answer_b.style.display = "block";
+    answer_c.style.display = "block";
+    next_question_btn.style.display = "block";
     if (q_idx === 10) {
         q_idx = 0;
         make_request(curQuery, new_article_query);
     } else {
         question_body.innerHTML = data['questions'][q_idx][1];
+        label =  data['questions'][q_idx][0];
+        answers = [];
+        correct_answer = data['questions'][q_idx][2];
+        answers.push(correct_answer);
+        answers.push(get_wrong_answer(label));
+        answers.push(get_wrong_answer(label));
+        shuffle(answers);
+
+        answer_a.innerHTML = answers[0];
+        answer_b.innerHTML = answers[1];
+        answer_c.innerHTML = answers[2];
+
         q_idx += 1;
+    }
+}
+
+function get_wrong_answer(label) {
+    if (label === "LOCATION") {
+        return randomFromArr(data['locations']);
+    } else if (label === "PROPER") {
+        return randomFromArr(data['propers']);
+    } else {
+        return randomFromArr(data['numbers']);
+    }
+}
+
+function randomFromArr(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function answered_a() {
+    console.log(correct_answer);
+    console.log(answer_a.innerHTML);
+    if (correct_answer === answer_a.innerHTML){
+        answer_response_label.innerHTML = "Correct!";
+    } else {
+        answer_response_label.innerHTML = "That's wrong...";
+    }
+    answer_response_label.style.display = "block";
+}
+
+function answered_b() {
+    if (correct_answer === answer_b.innerHTML){
+        answer_response_label.innerHTML = "Correct!";
+    } else {
+        answer_response_label.innerHTML = "That's wrong...";
+    }
+    answer_response_label.style.display = "block";
+}
+
+function answered_c() {
+    if (correct_answer === answer_c.innerHTML){
+        answer_response_label.innerHTML = "Correct!";
+    } else {
+        answer_response_label.innerHTML = "That's wrong...";
+    }
+    answer_response_label.style.display = "block";
+}
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items The array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
     }
 }
 
