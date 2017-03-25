9278abd73fb11351bfdff21aef40ebf109e178fe
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-03-25 16:24:21 -0700

    Remove copy/pasted sampling code

--- python/Quiz.py
+++ python/Quiz.py
@@ -20,34 +20,13 @@ class Quiz():
     # are used to give a pool of wrong answers
     # to the browser
     def get_random_propers(self):
-        if len(self.propers) == 0:
-            return []
-
-        selected = []
-        for i in range(0,30):
-            selected.append(random.choice(self.propers))
-
-        return selected
+        return _sample(self.propers, 30)
 
     def get_random_locations(self):
-        if len(self.locations) == 0:
-            return []
-
-        selected = []
-        for i in range(0,30):
-            selected.append(random.choice(self.locations))
-
-        return selected
+        return _sample(self.locations, 30)
 
     def get_random_numbers(self):
-        if len(self.numbers) == 0:
-            return []
-
-        selected = []
-        for i in range(0,30):
-            selected.append(random.choice(self.numbers))
-
-        return selected
+        return _sample(self.numbers, 30)
 
     # when the parser/chunker matches a word/phrase
     # to our grammar, lets add it to the pool of potential
